95ba4d4e2af232c97341807267ce8bd54018d1e7
harsh-shaha <56432306+harsh-shaha@users.noreply.github.com>
2021-06-07 18:10:03 +0800

    Game

--- Build/UnityLoader.js
+++ Build/UnityLoader.js
@@ -2064,7 +2064,7 @@ var UnityLoader = UnityLoader || {
             text: "OK",
             callback: r
         }])*/
-      t();
+        t();
     },
     buildCompatibilityCheck: function(e, t, r) {
         function n() {
@@ -2328,11 +2328,7 @@ var UnityLoader = UnityLoader || {
                 for (var s in r[a]) i.Module[s] = r[a][s];
             else i[a] = r[a];
         return n(e, i) || document.addEventListener("DOMContentLoaded", function() {
-          let unityContent = document.getElementById('unityContainer');
-          var tempWidth = window.innerWidth ;
-          var tempHeight = window.innerHeight;
-          unityContent.style.width = tempWidth+'px';
-          unityContent.style.height = tempHeight+'px';
+            let unityContent = document.getElementById('unityContainer');var tempWidth = window.innerWidth ;var tempHeight = window.innerHeight;unityContent.style.width = tempWidth+'px';unityContent.style.height = tempHeight+'px';
             n(e, i)
         }), i
     },
